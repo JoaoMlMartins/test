67487d6b48941c471cd4e626f4120624e41a726f
João Martins <a64537@ualg.pt>
2022-08-03 17:15:17 +0100

    new mod method

--- src/Utils.java
+++ src/Utils.java
@@ -14,10 +14,16 @@ public class Utils {
     public static void morcon() {
         System.out.println("MOOOOORCON!");
     }
+
     public static int div(int x, int y) {
         return x / y;
     }
+
     public static void augh() {
         System.out.println("AUGH!");
+    }
+
+    public static int mod( int x, int y){
+        return x % y;
     }
 }
